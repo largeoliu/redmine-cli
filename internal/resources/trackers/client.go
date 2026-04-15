cd9c0d73a83a11b1c64d46576f2391f383e33e18
liuhao <largeoliu@gmail.com>
2026-04-15 23:14:43 +0800

    feat(tracker): add get command with custom fields support

--- internal/resources/trackers/client.go
+++ internal/resources/trackers/client.go
@@ -3,25 +3,33 @@ package trackers
 
 import (
 	"context"
+	"strconv"
 
 	"github.com/largeoliu/redmine-cli/internal/client"
 )
 
-// Client is a client for managing Redmine trackers.
 type Client struct {
 	client *client.Client
 }
 
-// NewClient creates a new trackers client.
 func NewClient(c *client.Client) *Client {
 	return &Client{client: c}
 }
 
-// List retrieves all trackers.
 func (c *Client) List(ctx context.Context) (*TrackerList, error) {
 	var result TrackerList
 	if err := c.client.Get(ctx, "/trackers.json", &result); err != nil {
 		return nil, err
 	}
 	return &result, nil
+}
+
+func (c *Client) Get(ctx context.Context, id int) (*Tracker, error) {
+	var result struct {
+		Tracker *Tracker `json:"tracker"`
+	}
+	if err := c.client.Get(ctx, "/trackers/"+strconv.Itoa(id)+".json", &result); err != nil {
+		return nil, err
+	}
+	return result.Tracker, nil
 }
