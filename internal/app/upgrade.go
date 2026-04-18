43119600d1a44f6bfab6dab4b083100e774eefb9
liuhao <largeoliu@gmail.com>
2026-04-19 07:24:43 +0800

    fix(upgrade): use nolint directives for errcheck
    - Replace _ = pattern with //nolint:errcheck directives
      since golangci-lint v2 check-blank: true flags _ =
    - Add nolint directives for deferred Close() calls
      where errors are not actionable
    - Add nolint directives for strconv.Atoi where
      default zero value on error is intentional
    - Add nolint directives for cleanup operations
      (RemoveAll, Remove) in error paths
    - Fix gofmt formatting

--- internal/app/upgrade.go
+++ internal/app/upgrade.go
@@ -1,4 +1,3 @@
-// Package app provides the CLI application commands and logic.
 package app
 
 import (
@@ -106,7 +105,7 @@ func runUpgrade(checkOnly bool, targetVersion string) error {
 	if err != nil {
 		return err
 	}
-	defer binary.Close()
+	defer binary.Close() //nolint:errcheck
 
 	exePath, err := osExecutableFunc()
 	if err != nil {
@@ -133,7 +132,7 @@ func fetchLatestRelease() (*githubRelease, error) {
 	if err != nil {
 		return nil, errors.NewNetwork("failed to fetch release info", errors.WithCause(err))
 	}
-	defer resp.Body.Close()
+	defer resp.Body.Close() //nolint:errcheck
 
 	if resp.StatusCode != http.StatusOK {
 		return nil, errors.NewNetwork(
@@ -183,10 +182,10 @@ func compareVersions(current, latest string) int {
 	for i := 0; i < maxLen; i++ {
 		var c, l int
 		if i < len(currentParts) {
-			c, _ = strconv.Atoi(currentParts[i])
+			c, _ = strconv.Atoi(currentParts[i]) //nolint:errcheck
 		}
 		if i < len(latestParts) {
-			l, _ = strconv.Atoi(latestParts[i])
+			l, _ = strconv.Atoi(latestParts[i]) //nolint:errcheck
 		}
 		if c < l {
 			return -1
@@ -204,7 +203,7 @@ func downloadAndExtract(url, goos string) (io.ReadCloser, error) {
 	if err != nil {
 		return nil, errors.NewNetwork("failed to download release archive", errors.WithCause(err))
 	}
-	defer resp.Body.Close()
+	defer resp.Body.Close() //nolint:errcheck
 
 	if resp.StatusCode != http.StatusOK {
 		return nil, errors.NewNetwork(
@@ -220,16 +219,16 @@ func downloadAndExtract(url, goos string) (io.ReadCloser, error) {
 	archivePath := filepath.Join(tmpDir, "archive")
 	f, err := osCreateFunc(archivePath)
 	if err != nil {
-		_ = os.RemoveAll(tmpDir)
+		os.RemoveAll(tmpDir) //nolint:errcheck
 		return nil, errors.NewInternal("failed to create temp file", errors.WithCause(err))
 	}
 
 	if _, copyErr := ioCopyFunc(f, resp.Body); copyErr != nil {
-		_ = f.Close()
-		_ = os.RemoveAll(tmpDir)
+		f.Close()            //nolint:errcheck
+		os.RemoveAll(tmpDir) //nolint:errcheck
 		return nil, errors.NewNetwork("failed to write archive", errors.WithCause(copyErr))
 	}
-	_ = f.Close()
+	f.Close() //nolint:errcheck
 
 	var binaryPath string
 
@@ -241,13 +240,13 @@ func downloadAndExtract(url, goos string) (io.ReadCloser, error) {
 	}
 
 	if err != nil {
-		_ = os.RemoveAll(tmpDir)
+		os.RemoveAll(tmpDir) //nolint:errcheck
 		return nil, errors.NewInternal("failed to extract archive", errors.WithCause(err))
 	}
 
 	binaryFile, err := osOpenFunc(binaryPath)
 	if err != nil {
-		_ = os.RemoveAll(tmpDir)
+		os.RemoveAll(tmpDir) //nolint:errcheck
 		return nil, errors.NewInternal("failed to open extracted binary", errors.WithCause(err))
 	}
 
@@ -261,7 +260,7 @@ type cleanupReadCloser struct {
 
 func (c *cleanupReadCloser) Close() error {
 	err := c.ReadCloser.Close()
-	_ = os.RemoveAll(c.cleanupDir)
+	os.RemoveAll(c.cleanupDir) //nolint:errcheck
 	return err
 }
 
@@ -270,13 +269,13 @@ func extractTarGz(archivePath, destDir string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	defer func() { _ = f.Close() }()
+	defer f.Close() //nolint:errcheck
 
 	gzr, err := gzipNewReaderFunc(f)
 	if err != nil {
 		return "", err
 	}
-	defer func() { _ = gzr.Close() }()
+	defer gzr.Close() //nolint:errcheck
 
 	tr := tar.NewReader(gzr)
 
@@ -301,10 +300,10 @@ func extractTarGz(archivePath, destDir string) (string, error) {
 				return "", err
 			}
 			if _, err := ioCopyFunc(outFile, io.LimitReader(tr, 100*1024*1024)); err != nil {
-				_ = outFile.Close()
+				outFile.Close() //nolint:errcheck
 				return "", err
 			}
-			_ = outFile.Close()
+			outFile.Close() //nolint:errcheck
 			return outPath, nil
 		}
 	}
@@ -317,7 +316,7 @@ func extractZip(archivePath, destDir string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	defer func() { _ = r.Close() }()
+	defer r.Close() //nolint:errcheck
 
 	for _, f := range r.File {
 		name := filepath.Base(f.Name)
@@ -333,16 +332,16 @@ func extractZip(archivePath, destDir string) (string, error) {
 			}
 			rc, err := f.Open()
 			if err != nil {
-				_ = outFile.Close()
+				outFile.Close() //nolint:errcheck
 				return "", err
 			}
 			if _, err := ioCopyFunc(outFile, io.LimitReader(rc, 100*1024*1024)); err != nil {
-				_ = rc.Close()
-				_ = outFile.Close()
+				rc.Close()      //nolint:errcheck
+				outFile.Close() //nolint:errcheck
 				return "", err
 			}
-			_ = rc.Close()
-			_ = outFile.Close()
+			rc.Close()      //nolint:errcheck
+			outFile.Close() //nolint:errcheck
 			return outPath, nil
 		}
 	}
@@ -359,21 +358,21 @@ func replaceBinary(currentPath string, newBinary io.Reader) error {
 	tmpPath := tmpFile.Name()
 
 	if _, err := ioCopyFunc(tmpFile, newBinary); err != nil {
-		_ = tmpFile.Close()
-		_ = os.Remove(tmpPath)
+		tmpFile.Close()    //nolint:errcheck
+		os.Remove(tmpPath) //nolint:errcheck
 		return errors.NewInternal("failed to write new binary", errors.WithCause(err))
 	}
 
 	if err := osChmodFunc(tmpFile, 0755); err != nil {
-		_ = tmpFile.Close()
-		_ = os.Remove(tmpPath)
+		tmpFile.Close()    //nolint:errcheck
+		os.Remove(tmpPath) //nolint:errcheck
 		return errors.NewInternal("failed to set permissions on new binary", errors.WithCause(err))
 	}
 
-	_ = tmpFile.Close()
+	tmpFile.Close() //nolint:errcheck
 
 	if err := os.Rename(tmpPath, currentPath); err != nil {
-		_ = os.Remove(tmpPath)
+		os.Remove(tmpPath) //nolint:errcheck
 		return errors.NewInternal(
 			"failed to replace binary",
 			errors.WithCause(err),
